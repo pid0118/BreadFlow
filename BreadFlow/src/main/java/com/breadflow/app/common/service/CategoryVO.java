69f9540fbc70abafe3d81d203095a87db53afc91
kdk8672 <kdk8672@naver.com>
2024-11-15 17:10:39 +0900

    2024-11-15 - 공통 부분(카테고리) 추가 - 발주 요청 모달 수정

---
+++ BreadFlow/src/main/java/com/breadflow/app/common/service/CategoryVO.java
@@ -0,0 +1,9 @@
+package com.breadflow.app.common.service;
+
+import lombok.Data;
+
+@Data
+public class CategoryVO {
+	private String major;
+	private String sub;
+}
---
+++ BreadFlow/src/main/java/com/breadflow/app/common/web/CategoryController.java
@@ -0,0 +1,25 @@
+package com.breadflow.app.common.web;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.breadflow.app.common.service.CategoryService;
+import com.breadflow.app.common.service.CategoryVO;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class CategoryController {
+	private final CategoryService categoryService;
+	
+	@GetMapping("category/list")
+	@ResponseBody
+	public List<CategoryVO> categoryList() {
+		return categoryService.getCategories();
+	}
+	
+}
