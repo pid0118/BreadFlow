91b6897ba63924952b76a69e361a9e200bfa0088
kdk8672 <kdk8672@naver.com>
2024-11-21 18:18:53 +0900

    2024-11-21 - 품목 추가 옵션 리셋 - 납기 희망 일자 미입력시 발주 신청 이벤트 x - 발주 신청 정상 작동 - 체크 박스 전체 선택

--- BreadFlow/src/main/java/com/breadflow/app/ordering/service/OrderingService.java
+++ BreadFlow/src/main/java/com/breadflow/app/ordering/service/OrderingService.java
@@ -1,8 +1,9 @@
 package com.breadflow.app.ordering.service;
 
 import java.util.List;
+import java.util.Map;
 
 public interface OrderingService {
 	public List<OrderingVO> selectOrderingList();
-	public int insertOrdering(OrderingVO orderingVO, List<OrderingDetailVO> orderingDetailVO);
+	public int insertOrdering(Map<String, Object> ordering);
 }
